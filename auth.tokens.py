07fc00934fcc1897baa523c9c6671389cadd830b
Tyler Corsair <tyler@tylercorsair.com>
2021-02-27 12:09:40 -0800

    Update to include fixes and improvements for auth changes.

--- auth.tokens.py
+++ auth.tokens.py
@@ -6,32 +6,24 @@
 # This modified version has been distributed with permission.
 # For more information on the process, please review the comments below.
 
+import argparse
 import base64
 import hashlib
 import os
 import re
-import random
 import time
-import argparse
 import json
 from urllib.parse import parse_qs
+
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-# Defining some variables we'll use later on.
-# The CLIENT ID provided is utilized by the mobile app and third-party services to ensure tokens are refreshable.
-MAX_ATTEMPTS = 3
+MAX_ATTEMPTS = 30
 CLIENT_ID = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
-UA = "SSO Token Generation"
-X_TESLA_USER_AGENT = "SSO Token Generation"
 
-# Setting up the ability to receive parameters via command line.
-parser = argparse.ArgumentParser(description='Provide credentials for Tesla authentication.')
-parser.add_argument('-u')
-parser.add_argument('-p')
-parser.add_argument('-c')
-args = parser.parse_args()
-
-# Generates random strings used for validation purposes and is standard with OAuth2.
 def gen_params():
     verifier_bytes = os.urandom(86)
     code_verifier = base64.urlsafe_b64encode(verifier_bytes).rstrip(b"=")
@@ -39,7 +31,6 @@ def gen_params():
     state = base64.urlsafe_b64encode(os.urandom(16)).rstrip(b"=").decode("utf-8")
     return code_verifier, code_challenge, state
 
-# This removes single quotes around the provided parameters to prevent some Python errors.
 def remove_quotes(string):
     if string.startswith("'") and string.endswith("'"):
         string = string[1:-1]
@@ -47,42 +38,52 @@ def remove_quotes(string):
     else:
         return string
 
-# The main process of authenticating a Tesla account.
-# For the sake of simplicity, a email, password, and passcode is always required.
-def login(email, password, passcode):
+def create_driver():
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
+    driver.execute_cdp_cmd("Network.setUserAgentOverride", {"userAgent": UA})
+    return driver
 
-    headers = {}
+def login(args):
+    # Set up some random parameters we'll use later.
+    email, password = remove_quotes(args.email), remove_quotes(args.password)
+    vprint = print if args.v else lambda _: None
+    session, resp, params, code_verifier = (None,) * 4
+    code_verifier, code_challenge, state = gen_params()
 
-    # [1] The first process of authenticating is to request the Login form on Tesla.com and receive some
-    # variables to send with a subsequent request. This is essential for our login attempt to succeed.
-    for attempt in range(MAX_ATTEMPTS):
-        code_verifier, code_challenge, state = gen_params()
+    # Set the default headers that we'll reuse a few times.
+    default_headers = {}
 
-        # GET parameters that are required for the login request to continue are prepared.
-        params = (
-            ("client_id", "ownerapi"),
-            ("code_challenge", code_challenge),
-            ("code_challenge_method", "S256"),
-            ("redirect_uri", "https://auth.tesla.com/void/callback"),
-            ("response_type", "code"),
-            ("scope", "openid email offline_access"),
-            ("state", state),
-        )
+    # Set the default params; many endpoints will require this to avoid a 400 error (Bad Request)
+    params = (
+        ("audience", ""),
+        ("client_id", "ownerapi"),
+        ("code_challenge", code_challenge),
+        ("code_challenge_method", "S256"),
+        ("locale", "en"),
+        ("prompt", "login"),
+        ("redirect_uri", "https://auth.tesla.com/void/callback"),
+        ("response_type", "code"),
+        ("scope", "openid email offline_access"),
+        ("state", state),
+    )
 
-        # A new session is started; this will carry over the cookie from this request to use with subsequent requests.
-        session = requests.Session()
-        resp = session.get("https://auth.tesla.com/oauth2/v3/authorize", headers=headers, params=params)
-        if resp.ok and "<title>" in resp.text:
-            break
-        time.sleep(3)
-    else:
-        return print(f"An error occurred when making a request to Tesla.")
+    # [1] Login Form
+    session = requests.Session()
+    driver = create_driver()
+    driver.get(f"https://auth.tesla.com/oauth2/v3/authorize?audience=&state={state}&code_challenge={code_challenge}&prompt=login&code_challenge_method=S256&client_id=ownerapi&redirect_uri=https%3A%2F%2Fauth.tesla.com%2Fvoid%2Fcallback&locale=en-US&scope=openid+email+offline_access&audience=owenerapi&response_type=code")
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name=identity]")))
 
-    # [2] Everything is clear, so lets get those variables we mentioned above.
-    # Once we've extracted them, we'll insert them into the data object below including the provided credentials.
-    csrf = re.search(r'name="_csrf".+value="([^"]+)"', resp.text).group(1)
-    transaction_id = re.search(r'name="transaction_id".+value="([^"]+)"', resp.text).group(1)
+    # inject browser cookies to requests.Session
+    for cookie in driver.get_cookies():
+        session.cookies.set(cookie["name"], cookie["value"])
 
+    csrf = driver.find_element_by_css_selector("input[name=_csrf]").get_attribute("value")
+    transaction_id = driver.find_element_by_css_selector("input[name=transaction_id]").get_attribute("value")
+    driver.quit()
+
+    # Now that we've extracted the CRSF and transaction_id, lets build our form data.
     data = {
         "_csrf": csrf,
         "_phase": "authenticate",
@@ -93,54 +94,109 @@ def login(email, password, passcode):
         "credential": password,
     }
 
+    # [2] Attempt to login.
+    # We can experience a handful of possible outcomes here as discussed below.
     for attempt in range(MAX_ATTEMPTS):
-        resp = session.post(
-            "https://auth.tesla.com/oauth2/v3/authorize", headers=headers, params=params, data=data, allow_redirects=False
-        )
-        if resp.ok and (resp.status_code == 302 or "<title>" in resp.text):
-            if not "Your account has been locked" in resp.text:
-                break
-            else:
-                return print(f"Your Tesla Account is locked and requires its password to be reset on Tesla.com.")
+        try:
+            resp = session.post("https://auth.tesla.com/oauth2/v3/authorize", headers=default_headers, params=params, data=data, allow_redirects=False, timeout=1)
+
+            # We triggered Tesla's WAF (Web Application Firewall), so lets try again.
+            if resp.ok and "support ID" in resp.text:
+                vprint(f"Got bot detection. Will try again.")
+
+            # We were redirected somewhere, so lets determine where that will take us.
+            if resp.ok and (resp.status_code == 302 or "<title>" in resp.text):
+
+                # We were successful at logging in, so lets move to the next step.
+                if not "Your account has been locked" in resp.text:
+                    vprint(f"Post auth form success - {attempt + 1} attempt(s).")
+                    break
+                else:
+                    raise print(json.dumps({"code": 420, "response": "Your Tesla Account is locked and requires its password to be reset on Tesla.com. This may have occurred due to repeatedly providing the wrong credentials or another third-party service you use not being updated to the latest authentication system."}))
+
+            # Credentials are incorrect.
+            if "We could not sign you in" in resp.text and resp.status_code == 401:
+                raise print(json.dumps({"code": 401, "response": "The credentials you provided were not valid."}))
+
+        except requests.exceptions.Timeout as e:
+            vprint('Request timed out. Trying again.')
         time.sleep(1)
     else:
-        return print(f"The credentials you provided were not valid.")
+        raise print(json.dumps({"code": 500, "response": "An error occurred when making a request to Tesla. Please try again or let our team know."}))
 
     # Determine if user has MFA enabled
-    # In that case there is no redirect to `https://auth.tesla.com/void/callback` and app shows new form with Passcode / Backup Passcode field
     is_mfa = True if resp.status_code == 200 and "/mfa/verify" in resp.text else False
 
-    # [3a] If the account has MFA enabled, we'll have to make an additional request and utilize the provided passcode.
     if is_mfa:
-        resp = session.get(
-            f"https://auth.tesla.com/oauth2/v3/authorize/mfa/factors?transaction_id={transaction_id}", headers=headers,
-        )
-        factor_id = resp.json()["data"][0]["id"]
+        # [3] If the account has MFA enabled, we'll attempt to get the device/app they set up.
+        # We will need this for the following steps.
+        for attempt in range(MAX_ATTEMPTS):
+            try:
+                resp = session.get(f"https://auth.tesla.com/oauth2/v3/authorize/mfa/factors?transaction_id={transaction_id}", headers=default_headers, timeout=1)
 
-        # Can use Passcode
-        data = {"transaction_id": transaction_id, "factor_id": factor_id, "passcode": passcode}
-        resp = session.post("https://auth.tesla.com/oauth2/v3/authorize/mfa/verify", headers=headers, json=data)
-        if "error" in resp.text or not resp.json()["data"]["approved"] or not resp.json()["data"]["valid"]:
-            return print(f"The passcode you provided was invalid.")
+                # We triggered Tesla's WAF (Web Application Firewall), so lets try again.
+                if resp.ok and "support ID" in resp.text:
+                    vprint(f"Got bot detection. Will try again.")
 
+                # We successfully got the MFA device, so we'll set its value and continue onward.
+                if resp.ok and resp.json()["data"][0]["id"] is not None:
+
+                    factor_id = resp.json()["data"][0]["id"]
+
+                    vprint(f"Got MFA factors, continuing onward. - {attempt + 1} attempt(s).")
+                    break
+            except requests.exceptions.Timeout as e:
+                vprint('Request timed out. Trying again.')
+            time.sleep(1)
+        else:
+            raise print(json.dumps({"code": 500, "response": "An error occurred when making a request to Tesla to obtain MFA devices. If this continues, please let our team know."}))
+
+        # Let's set up some special headers to prepare for our actual MFA verification request below.
+        headers = {}
+
+        # Set up our data that we've extracted in previous requests + the passcode.
+        data = {"transaction_id": transaction_id, "factor_id": factor_id, "passcode": remove_quotes(args.c)}
+
+        # [4] Let's attempt to verify the MFA passcode. Fingers crossed by the time we get here it hasn't expired.
+        for attempt in range(MAX_ATTEMPTS):
+            try:
+                resp = session.post("https://auth.tesla.com/oauth2/v3/authorize/mfa/verify", headers=headers, json=data, timeout=1)
+
+                # We successfully verified the passcode so let's continue on!
+                if(resp.json()["data"]["valid"]):
+                    vprint(f"MFA was valid, continuing onward. - {attempt + 1} attempt(s).")
+                    break
+
+                # The passcode was incorrect or invalid (expired).
+                if "error" in resp.text or not resp.json()["data"]["approved"] or not resp.json()["data"]["valid"]:
+                    vprint(resp.json())
+                    raise print(json.dumps({"code": "401", "response": "The passcode you provided was invalid."}))
+            except requests.exceptions.Timeout as e:
+                vprint('Request timed out. Trying again.')
+        else:
+            raise print(json.dumps({"code": 500, "response": "An error occurred when making a request to Tesla with your MFA. If this continues, please let our team know."}))
+
+        # Set up the data and headers for our following step.
         data = {"transaction_id": transaction_id}
 
+        headers = {}
+
+        # [5] Now that we've successfully authenticated and verified our MFA (if needed), lets get an auth code.
         for attempt in range(MAX_ATTEMPTS):
-            resp = session.post(
-                "https://auth.tesla.com/oauth2/v3/authorize",
-                headers=headers,
-                params=params,
-                data=data,
-                allow_redirects=False,
-            )
-            if resp.headers.get("location"):
-                break
+            try:
+                resp = session.post("https://auth.tesla.com/oauth2/v3/authorize", headers=headers, params=params, data=data, allow_redirects=False, timeout=1)
+                # We succeeded and got a "location", so lets grab that authorization code and continue onward.
+                if resp.headers.get("location"):
+                    vprint(f"Got authorization code in {attempt + 1} attempt(s).")
+                    break
+            except requests.exceptions.Timeout as e:
+                vprint('Request timed out. Trying again.')
         else:
-            return print(f"Something went wrong with the authentication process. Please try again.")
+            raise print(json.dumps({"code": 500, "response": "Something went wrong with the authentication process. Please try again."}))
 
     code = parse_qs(resp.headers["location"])["https://auth.tesla.com/void/callback?code"]
 
-    headers = {}
+    # We're almost there. Let's set up one of our final payloads.
     payload = {
         "grant_type": "authorization_code",
         "client_id": "ownerapi",
@@ -149,11 +205,59 @@ def login(email, password, passcode):
         "redirect_uri": "https://auth.tesla.com/void/callback",
     }
 
-    # [4] Lastly, we'll take the authorization code that we parsed above and send it and our previously
-    # generated code verifier to its final destination to generate our SSO refresh token.
-    resp = session.post("https://auth.tesla.com/oauth2/v3/token", headers=headers, json=payload)
-    refresh_token = resp.json()["refresh_token"]
-    print(f'Provide the following refresh token to the third-party service or app: {refresh_token}')
+    # [6] Lets take that authorization code and generate our SSO tokens! FINALLY.
+    for attempt in range(MAX_ATTEMPTS):
+        try:
+            resp = session.post("https://auth.tesla.com/oauth2/v3/token", headers=default_headers, json=payload, timeout=1)
+
+            # We got our SSO tokens. Let's grab the tokens that we'll use later.
+            if resp.ok and resp.json()["access_token"]:
+
+                sso_access_token = resp.json()["access_token"]
+                sso_refresh_token = resp.json()["refresh_token"]
+
+                vprint(f"Got SSO tokens in {attempt + 1} attempt(s).")
+                break
+        except requests.exceptions.Timeout as e:
+            vprint('Request timed out. Trying again.')
+        time.sleep(3)
+    else:
+        raise print(json.dumps({"code": 500, "response": "Something went wrong getting account tokens. Please try again."}))
+
+    # Set up the headers and payload for our final step.
+    default_headers["authorization"] = "bearer " + sso_access_token
+    payload = {
+        "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        "client_id": CLIENT_ID,
+    }
+
+    # [7] We got a SSO token so we can do things on the account's behalf now.
+    # Let's use this to generate an owners API token which allows us to access vehicle data.
+    for attempt in range(MAX_ATTEMPTS):
+        try:
+            resp = session.post("https://owner-api.teslamotors.com/oauth/token", headers=default_headers, json=payload)
+
+            # If the response contains an access token, then we're done!
+            if resp.json()["access_token"] is not None:
+                vprint(f"Got access token in {attempt + 1} attempt(s).")
+                break
+        except requests.exceptions.Timeout as e:
+            vprint('Request timed out. Trying again.')
+        time.sleep(3)
+    else:
+        raise print(json.dumps({"code": 500, "response": "Something went wrong getting SSO tokens. Please try again."}))
+
+    # Let's display their tokens.
+    print(json.dumps({"code": 200, "tokens": {"owner_access_token": resp.json()["access_token"], "sso_refresh_token": sso_refresh_token}}))
 
 if __name__ == "__main__":
-    login(remove_quotes(args.u), remove_quotes(args.p), remove_quotes(args.c or '111111'))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--email", required=True, help="Tesla account email")
+    parser.add_argument("-p", "--password", required=True, help="Tesla account password")
+    parser.add_argument("-v", required=False, action="store_true", help="Be verbose")
+
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument("-c", help="Passcode generated by your authenticator app")
+
+    args = parser.parse_args()
+    login(args)
